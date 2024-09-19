c32dbdc56538b0ef8483308545d9e284e3672e62
Muhammad Bilal <muhmdbilal3333@gmail.com>
2024-09-20 01:55:35 +0500

    wallet add

--- src/main.tsx
+++ src/main.tsx
@@ -29,6 +29,7 @@ const metadata = {
 const wagmiConfig = defaultWagmiConfig({
   projectId,
   chains,
+  
 });
 
 // Initialize Web3Modal
@@ -37,6 +38,13 @@ createWeb3Modal({
   projectId,
   wagmiConfig, // pass wagmiConfig correctly here
   metadata,
+  includeWalletIds: [
+    "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
+    '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
+    "971e689d0a5be527bac79629b4ee9b925e82208e5168b733496a09c0faed0709",
+    "8a0ee50d1f22f6651afcae7eb4253e52a3310b90af5daef78a8c4929a9bb99d4",
+    "15c8b91ade1a4e58f3ce4e7a0dd7f42b47db0c8df7e0d84f63eb39bcb96c4e0f"
+  ]
 });
 
 // Render the app
